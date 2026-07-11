5170eda4af8e8440ffa11d727ae913c67044cb5d
Tayeb <mohammad.tayeb9010@gmail.com>
2026-07-11 15:08:23 +0600

    hero carosal update

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -27,7 +27,7 @@ const slides = [
 
 const HeroCarousel = () => {
   return (
-    <div className="relative w-full md:max-w-7xl mx-auto h-[200px] sm:h-[280px] md:h-[400px] lg:h-[500px] overflow-hidden sm:rounded-2xl md:shadow-2xl bg-black">
+    <div className="relative w-full md:max-w-7xl mx-auto h-[165px] sm:h-[280px] md:h-[400px] lg:h-[500px] overflow-hidden bg-black">
       <Swiper
         modules={[Navigation, Pagination, Autoplay, EffectFade]}
         effect="fade"
@@ -56,7 +56,7 @@ const HeroCarousel = () => {
               alt="banner images"
               fill
               priority={slide.id === 1}
-              className="object-cover object-center"
+              className="object-center md:object-cover object-contain"
             />
           </SwiperSlide>
         ))}
