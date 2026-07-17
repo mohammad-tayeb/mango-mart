354c9175718464e7ba8634e5365c4cdd80311630
Tayeb <mohammad.tayeb9010@gmail.com>
2026-07-17 16:43:19 +0600

    products ui update

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -19,7 +19,7 @@ export default async function Products({ searchParams }) {
             <div className="mb-10">
                 {products.length > 0 ? (
                     <>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 mt-4 md:grid-cols-3 lg:grid-cols-4 gap-6 px-6">
+                        <div className="grid grid-cols-2 sm:grid-cols-2 mt-4 md:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-6 px-3">
                             {products.map((product) => (
                                 <ProductCard
                                     key={product._id}
